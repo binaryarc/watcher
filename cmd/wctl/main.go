bd4b558c83e629f0195258ef0398035d4c205df2
이진호 <jjin4363@gmail.com>
2025-12-11 19:00:11 +0900

    feat: Setup Cobra CLI Project structure

--- cmd/wctl/main.go
+++ cmd/wctl/main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/binaryarc/watcher/cmd"
+import "github.com/binaryarc/watcher/pkg/cmd"
 
 func main() {
 	cmd.Execute()
